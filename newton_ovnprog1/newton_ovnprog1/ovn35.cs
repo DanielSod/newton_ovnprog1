a4b084bcfa6c2a28a1650c66d99af361e34ce9ae
DanielSod <dansod85@gmail.com>
2019-09-19 12:56:57 +0200

    jobbar på ovn35

---
+++ newton_ovnprog1/newton_ovnprog1/ovn35.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace newton_ovnprog1
+{
+    class ovn35
+    {
+        public void  printpwrtoall()
+        {
+
+
+
+            
+            
+
+            Console.WriteLine("Kvadraten av {0} är: {1}", i, num1);
+
+            pwrtoall(i);
+            
+        }
+                   
+        public void pwrtoall(int i)
+        {
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (i = 10)
+                    break;
+
+            }
+
+    
+        }
+                  
+
+    }
+}
