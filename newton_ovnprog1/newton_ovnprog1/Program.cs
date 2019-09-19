4ad12cbad2ff88c3dddffa2ea1364520f4f1e4ab
DanielSod <dansod85@gmail.com>
2019-09-19 02:21:15 +0200

    nuu

---
+++ newton_ovnprog1/newton_ovnprog1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace newton_ovnprog1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ovn11 o11 = new ovn11();
+            o11.printhej();
+
+
+        }
+    }
+}
